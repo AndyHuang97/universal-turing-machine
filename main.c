93800156c94b493cb99e210b797e3a79342e5cba
AndyHuang97 <37264030+AndyHuang97@users.noreply.github.com>
2018-05-24 20:29:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ typedef struct transition{
 typedef struct machine{
     transition *tr;
     int *Accept;
-    config snapshot;	//memorizes the OC's current state and input string's head
+    config snapshot;//memorizes the OC's current state and input string's head
     int max;
 }TM;
 
@@ -25,9 +25,11 @@ void Load_MT(FILE);
 
 int main(int argc, const char * argv[]) {
     char input[25] = "input.txt";
-    char *Line = (char *)malloc(sizeof(char)); 
+    
+    char *Line = (char *)malloc(sizeof(char)); //line ha un indirizzo, quindi ci posso salvare stringhe
     transition *tr = (transition *) malloc(sizeof(transition));
-    int i;
+    //int i;
+    char *car = (char *)malloc(sizeof(char));
     FILE *fp;
     
     printf("input file: %s\n", input);
@@ -37,21 +39,32 @@ int main(int argc, const char * argv[]) {
     else
     {
         printf("Success in opening input file!\n");
-        fgets(Line, 100, fp);       //legge tr
+        fgets(Line, 100, fp);       ////legge tr;
         printf("%s",Line);
-        
+        /*
+        for(i = 0; i < 3; i++)      //legge tr;
+        {
+            Let = fgetc(fp);
+            if(Let == '\n')
+                printf("beccato newline!\n");
+            printf("%d - %c\n", i, Let);
+        }*/
+        //Let = fgetc(fp);  //legge primo stato (int)
         fgets(Line, 100, fp);
-        
-        while(strcmp(Line, "acc\n")){
-            tr->init = *Line;   //da modificare: conversione in int
+        while(strcmp(Line, "acc\n")){       //mancaono tutti gli hashtables
+            *car = *Line;
+            tr->init = atoi(car);       //int
+            printf("-%d", tr->init);
             tr->in = *(Line + 2);
             tr->out = *(Line + 4);
             tr->head = *(Line + 6);
-            tr->final = *(Line + 8);    //da modificare: conversione in int
+            *car = *(Line + 8);
+            tr->final = atoi(car);     //int
+            printf("-%d ", tr->final);
             printf("%s", Line);
             fgets(Line, 100, fp);
         }
-        
+
         printf("END\n");
     }
     return 0;
