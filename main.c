d24444007da99f1c0ce7a94fb7cba9609f181856
AndyHuang97 <37264030+AndyHuang97@users.noreply.github.com>
2018-05-26 15:08:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
+/*
+ Description: Simulator of a Nondeterministic Turing Machine;
+ Usage: ./tm <inputfile> <outputfile>
+ 
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+//------------------------------------------------------------
+//TM's data structures
 typedef struct configuration{
     int state;          //state of the OC;
     char *InStr;        //input string;
@@ -29,8 +35,30 @@ typedef struct machine{
     int *Accept;    //array of final states;
     int max;        //condition for termination if it's looping
 }TM;
-
+//------------------------------------------------------------
+//sorting data structures
+typedef struct Heap{
+    int start;
+    char input;
+    char output;
+    char hmove;
+    int end;
+}Heap;
+typedef struct array{
+    int *heapArray;
+    int Length;
+    int heapSize;
+}array;
+//------------------------------------------------------------
 void init_TM(TM *);
+int PARENT(int);
+int LEFT(int);
+int RIGHT(int);
+void MAX_HEAPIFY(array *, int, char **);
+void BUILD_MAX_HEAP(array *, char **);
+void HeapSort1(array *, char **);
+
+void sortTr(char *);
 void Load_TM(TM *, char *);
 void createSt(State *);
 
@@ -39,6 +67,143 @@ void init_TM(TM *tm)
     printf("Initializing the TM ...\n\n");
     tm->tr = (State *)malloc(sizeof(State));   //creates tm
     tm->Accept = (int *)malloc(sizeof(int));
+}
+//HEAPSORT function and support functions
+int PARENT(int i){
+    return i/2;
+}
+int LEFT(int i){
+    return 2*i;
+}
+int RIGHT(int i){
+    return 2*i + 1;
+}
+void MAX_HEAPIFY(array *A, int i, char **line){
+    int max;
+    char* tempLine;
+    int temp;
+    int l = LEFT(i);
+    int r = RIGHT(i);
+    
+    if(l <= A->heapSize && A->heapArray[l] > A->heapArray[i])
+        max = l;
+    else max = i;
+    if(r <= A->heapSize && A->heapArray[r] > A->heapArray[i])
+        max = r;
+    if( max != i)
+    {
+        //swap A[i] <-> A[max] ma anche di line
+        temp = A->heapArray[i];
+        tempLine = line[i];
+        A->heapArray[i] = A->heapArray[max];
+        line[i] = line[max];
+        A->heapArray[max] = temp;
+        line[max] = tempLine;
+        //end swap.
+        MAX_HEAPIFY(A, max, line);
+    }
+}
+void BUILD_MAX_HEAP(array *A, char** line)
+{
+    int i;
+    A->heapSize = A->Length;
+    for(i = A->Length/2; i >= 1; i--)
+        MAX_HEAPIFY(A, i, line);
+}
+void HeapSort1(array *A, char **line)
+{
+    int i;
+    char* tempLine;
+    int temp;
+    BUILD_MAX_HEAP(A, line);
+    for(i = A->Length/2; i >= 0; i--)
+    {
+        temp = A->heapArray[0];
+        tempLine = line[0];
+        A->heapArray[0] = A->heapArray[i];
+        line[0] = line[i];
+        A->heapArray[i] = temp;
+        line[i] = tempLine;
+        MAX_HEAPIFY(A, 0, line);
+    }
+    
+}
+//HEAPSORT end.
+void sortTr(char *InputFile){
+    FILE *fp1, *fp2;
+    Heap mheap;
+    char outputFile[25] = "tr_sorted.txt";
+    char c;
+    int states = 0, i = 0;
+    array A;
+    char **heapLine = NULL;
+    char *Line = NULL;
+    
+    fp1 = fopen("input1.txt", "r");
+    if(fp1 == NULL)
+        printf("Unable to open input file...\n\nLoading aborted!\n\n");
+    else
+    {
+        printf("Input file: %s\n", InputFile);
+        printf("Success in opening input file!\n");
+        Line = (char *)malloc(sizeof(char));
+        fgets(Line, 100, fp1);       //first line of the file (tr)
+        printf("%s", Line);
+        
+        do
+        {
+            states++;
+            //printf("%d) ", states);
+            fscanf(fp1, "%d %c %c %c %d\n", &mheap.start, &mheap.input, &mheap.output, &mheap.hmove, &mheap.end);
+            //printf("%d %c %c %c %d\n", mheap.start, mheap.input, mheap.output, mheap.hmove, mheap.end);
+            c = fgetc(fp1);          //"first character of the line
+            if(c != 'a')
+                fseek(fp1, -1, SEEK_CUR);
+        }while(c != 'a');
+        
+        A.heapArray = (int *)malloc(states*sizeof(int));  //array of the positions of the lines
+        A.Length = states;
+        A.heapSize = states;
+        heapLine = (char **)malloc(states*sizeof(char *)); //array of associated lines
+        
+        fseek(fp1, 3, SEEK_SET);    //salto riga di tr;
+        do
+        {
+            i++;
+            //printf("%d) ", i);
+            fscanf(fp1, "%d", &A.heapArray[i-1]);
+            fseek(fp1, -1, SEEK_CUR);
+            heapLine[i-1] = (char *)malloc(sizeof(char));
+            fgets(heapLine[i-1], 100, fp1);
+            //printf("%d\t", A.heapArray[i-1]);
+            //printf("%s", heapLine[i-1]);
+            c = fgetc(fp1);          //first character of the line
+            if(c != 'a')
+                fseek(fp1, -1, SEEK_CUR);
+        }while(c != 'a');
+        // controllo per vedere se gli stati sono stati memorizzati per bene
+        for(i = 0; i < states; i++)
+        {
+            printf("%d) %d\t", i+1, A.heapArray[i]);
+            printf("%i:\t", (unsigned int) *(heapLine + i));
+            printf("%s\n", *(heapLine + i));
+        }
+        printf("\n---------------\n");
+        HeapSort1(&A, heapLine);
+        // controllo per vedere se gli stati sono stati memorizzati per bene
+        for(i = 0; i < states; i++)
+        {
+            printf("%d) %d\t", i+1, A.heapArray[i]);
+            printf("%s", heapLine[i]);
+        }
+    }
+    fp2 = fopen(outputFile, "w");
+    if(fp2 == NULL)
+        printf("Unable to open output file...\n\nLoading aborted!\n\n");
+    else
+    {
+        
+    }
 }
 void Load_TM(TM *tm, char *filename)           // loading TM from commandline
 {
@@ -95,6 +260,16 @@ void createSt(State *tr){
 }
 
 int main(int argc, char *argv[]) {
+    /*
+    if(argc > 3 || argc < 2)
+    {
+        printf("\nWARNING:  Invalid number of arguments!\n\n");
+        printf("Usage: ./tm < <inputfile> <outputfile>\n\n");
+        
+        return -1;
+    }*/
+    sortTr(argv[1]);
+    /*
     TM tm;
     State *tempTr;
     
@@ -103,6 +278,6 @@ int main(int argc, char *argv[]) {
     
     //createSt(tm.tr);                                    //creates tr
     tempTr = tm.tr;
-    
+    */
     return 0;
 }
