8afdaa1ce7648dd1c83039c7b802931879102181
AndyHuang97 <37264030+AndyHuang97@users.noreply.github.com>
2018-07-07 17:08:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 /* ---------------------------------------------------------------------- */
 
 typedef struct configuration{
@@ -37,14 +38,16 @@ typedef struct machine{
 
 /* ---------- Private function declarations ---------- */
 void init_TM(TM *);
-void initInputHash(inputHash **S);
-void createEndList(endList **base, char output, char hmove, int end);
-inputHash *LIST_SEARCH(State *L, char k);
+void initInputHash(inputHash **);
+endList *createEndList(char, char, int);
+void ENDLIST_INSERT(endList **, endList *);
+inputHash *LIST_SEARCH(State *, char);
 void LoadTM(TM *);
-void LIST_INSERT(State *L, inputHash *x);
-inputHash *INPUT_SEARCH(TM tm, int start, char input);      //solo per controllo
+void LIST_INSERT(State *, inputHash *);
+inputHash *INPUT_SEARCH(TM , int , char );      //solo per controllo
 
-config *ND_Step(config **c, TM tm, int *);
+config *MKconfig(void);
+config *ND_Step(config **, TM , int *);
 char *LOAD_STRING(void);
 
 /* ---------------------------------------------------------------------- */
@@ -56,14 +59,7 @@ void init_TM(TM *tm) {
     tm->tr[0]->input_next = NULL;
     tm->Accept = (int *)malloc(sizeof(int));//init list of accepted states
     tm->numAcc = 1;
-    tm->snapshot = (config *)malloc(sizeof(config));
-    tm->snapshot->String = NULL;  //no input string loaded
-    tm->snapshot->pos = -1;
-    tm->snapshot->next = NULL;
-    tm->snapshot->prev = NULL;
-    tm->snapshot->pos = 0;
-    tm->snapshot->state = 0;
-    //printf("Initialization complete!\n\n");
+    tm->snapshot = MKconfig();
 }
 
 /* ---------------------------------------------------------------------- */
@@ -76,14 +72,25 @@ void initInputHash(inputHash **S) {
 }
 
 /* ---------------------------------------------------------------------- */
-void createEndList(endList **base, char output, char hmove, int end) {
-    *base = (endList *) malloc(sizeof(endList));
-    (*base)->output = output;
-    (*base)->hmove = hmove;
-    (*base)->end = end;
-    (*base)->next = NULL;
+endList *createEndList(char output, char hmove, int end) {
+    endList *new = NULL;
+    new = (endList *) malloc(sizeof(endList));
+    new->output = output;
+    new->hmove = hmove;
+    new->end = end;
+    new->next = NULL;
+    return new;
 }
 
+/* ---------------------------------------------------------------------- */
+void ENDLIST_INSERT(endList **L, endList *el) {
+    if(*L == NULL)
+        *L = el;
+    else {
+        el->next = *L;
+        *L = el;
+    }
+}
 /* ---------------------------------------------------------------------- */
 //SEARCH...
 inputHash *LIST_SEARCH(State *L, char k) {
@@ -110,52 +117,53 @@ void LIST_INSERT(State *L, inputHash *x) {
 /* ---------------------------------------------------------------------- */
 void LoadTM(TM *tm) {
     char input, output, hmove, c, Line[4];
-    int start, end, states = 0;
+    int start, end, states = 0, max = 0;
     int i;
     //State **old_addr = tm->tr;
     inputHash *tempInputH;
+    endList *new = NULL;
 
-    //while(fgetc(stdin) != '\n'){}   //reads line with string "tr"
     fscanf(stdin, "%s", Line);
-    //printf("tr\n");
-
     do {
         //reads the lines of the transition function
         //printf("input data: ");
         fscanf(stdin, "%d %c %c %c %d\n", &start, &input, &output, &hmove, &end);
         //printf("%d %c %c %c %d\n", start, input, output, hmove, end);
-
-        if(start > states) {//if start>states then reallocate new memory for it
-            tm->tr = (State **)realloc(tm->tr, (start+2)*sizeof(State *));//realloc start+1 objects.
-            /*if(tm->tr != old_addr)
-            {
-                printf("States: %d\told: %x, new: %x \n", states, (unsigned int) old_addr, (unsigned int) tm->tr);
-                old_addr = tm->tr;
-            }*/
-            tm->tr[start+1] = NULL;//last object of dynamic array not to be realloced
-            i = start;
-            while(i > states)   //just malloc for the newly added states(cells)
-            {   //check from the last cell and allocates new memory for it. It is assumed that the first positions have already their pointers.
+        if(max < start || max < end)
+        {
+            if(start > end)
+                max = start;
+            else
+                max = end;
+        }
+        if(max > states) {//if start>states then reallocate new memory for it
+            tm->tr = (State **)realloc(tm->tr, (max+2)*sizeof(State *));//realloc max+1 objects.
+            tm->tr[max+1] = NULL;//last object of dynamic array not to be realloced
+            i = max;
+            while(i > states) {   //just malloc for the newly added states(cells)
+              //check from the last cell and allocates new memory for it. It is assumed that the first positions have already their pointers.
                 tm->tr[i] = (State *)malloc(sizeof(State));
                 tm->tr[i]->start = i;
                 tm->tr[i]->input_next = NULL;
                 i -= 1;
             }
-            states = start;
+            states = max;
         }
-
         tempInputH = LIST_SEARCH(tm->tr[start], input);
-        if(tempInputH == NULL)
-        {
-            initInputHash(&tempInputH); //init an inputHash el
+        if(tempInputH == NULL) {
+            initInputHash(&tempInputH); //init an inputHash element
             tempInputH->input = input;
             LIST_INSERT(tm->tr[start], tempInputH);
             //finishing one step of the tr function
-            createEndList(&(tm->tr[start]->input_next->end_next), output, hmove, end);
+            new = createEndList(output, hmove, end);
+            ENDLIST_INSERT(&(tm->tr[start]->input_next->end_next), new);
         }
         else
             if(tempInputH != NULL)  //finishing one step of the tr function
-                createEndList(&(tempInputH->end_next->next), output, hmove, end);
+            {
+                new = createEndList(output, hmove, end);
+                ENDLIST_INSERT(&(tempInputH->end_next), new);
+            }
         c = getc(stdin);            //used to recognize the "acc" line
         ungetc(c, stdin);           //puts back the read character
     }while(c != 'a');
@@ -163,7 +171,7 @@ void LoadTM(TM *tm) {
     fscanf(stdin, "%s", Line);      //"acc" line
     //printf("%s\n", Line);
     i = 0;                          //using it for accept states array
-    do{
+    do {
         fscanf(stdin, "%d\n", &(tm->Accept[i]));     //final state
         c = getc(stdin);            //used to recognize the "max" line
         ungetc(c, stdin);           //puts back the read character
@@ -174,7 +182,6 @@ void LoadTM(TM *tm) {
             tm->numAcc += 1;
         }
     }while(c != 'm');
-
     /*for(int count = 0; count < i; count++)
         printf("%d\n", tm->Accept[count]);*/
     fscanf(stdin, "%s\n", Line);     //"max" line
@@ -231,9 +238,9 @@ inputHash *INPUT_SEARCH(TM tm, int start, char input) {
 //initialize a new config
 config *MKconfig(){
     config *c = (config *)malloc(sizeof(config));
-    c->state = -1;
+    c->state = 0;
     c->String = NULL;
-    c->pos = -1;
+    c->pos = 0;
     c->next = NULL;
     c->prev = NULL;
      return c;
@@ -248,16 +255,19 @@ void ADDconfig(config *root, config *leaf){
     temp->next = leaf;
     leaf->prev = temp;
 }
-config *MergeConfig(config *config1, config *config2){
-    config *temp = config1;
-    if(config1 != NULL && config2 != NULL)
+config *MergeConfig(config **config1, config **config2){
+    config *temp = *config1;
+    if(*config1 != NULL && *config2 != NULL)
     {
         while(temp->next != NULL)
             temp = temp->next;
-        temp->next = config2;
-        config2->prev = temp;
+        temp->next = *config2;
+        (*config2)->prev = temp;
     }
-    return config1;
+    else
+        if(*config1 == NULL)
+            *config1 = *config2;
+    return *config1;
 }
 /* ---------------------------------------------------------------------- */
 void ND_Transition(TM *tm){
@@ -265,27 +275,28 @@ void ND_Transition(TM *tm){
     config *snapshot = tm->snapshot;
     config *old_addr = NULL, *merge1 = NULL, *merge2 = NULL;
     
-    while (i < tm->max && !accept && tm->snapshot != NULL) {
+    while (i <= tm->max && !accept && tm->snapshot != NULL) {
         snapshot = tm->snapshot;
         old_addr = snapshot->next;
-        printf("%d)\n",i);
+        //printf("%d)\n",i);
         merge1 = ND_Step(&snapshot, *tm, &accept);
         while(old_addr != NULL && !accept) {
             snapshot = old_addr;
             old_addr = snapshot->next;
             merge2 = ND_Step(&snapshot, *tm, &accept);
-            merge1 = MergeConfig(merge1, merge2);
+            merge1 = MergeConfig(&merge1, &merge2);
         }
         tm->snapshot = merge1;
         if(!accept)
             i++;
     }
     if(accept)
-        printf("1\n");
-    if(i == tm->max)
-        printf("U\n");
-    if(snapshot == NULL)
-        printf("0\n");
+        printf("1");
+    if(i == tm->max+1)
+        printf("U");
+    if(tm->snapshot == NULL)
+        printf("0");
+    tm->snapshot = MKconfig();
 }
 
 /* ---------------------------------------------------------------------- */
@@ -301,16 +312,13 @@ config *ND_Step(config **c, TM tm, int *accept){
     if(inputH != NULL) {
         choice = inputH->end_next;
         while(choice != NULL) {      //all possible steps on the input
-            //printf("sizeof(char) = %lu\n", sizeof(char));
-            //printf("strlen(c->String) = %lu\n",strlen(c->String));
-            printf("BEFORE: %s\tS:%d\tH:%d\n", (*c)->String, (*c)->state, (*c)->pos);
+            //printf("BEFORE:\t%s\tS:%d\tH:%d\n", (*c)->String, (*c)->state, (*c)->pos);
             string = (char *) malloc(strlen((*c)->String)+2);
-            //printf("%d",(int) strlen(c->String));
             memcpy(string, (*c)->String, strlen((*c)->String)+1);
             oneconfig = MKconfig();  //creates a config var to pass to tm at the end;
             
             for(int i = 0; i < tm.numAcc; i++) //checks if it's finale state;
-                if(choice->end == tm.Accept[i]){
+                if(choice->end == tm.Accept[i]) {
                     *accept = 1;
                     return *c;
                 }
@@ -328,9 +336,10 @@ config *ND_Step(config **c, TM tm, int *accept){
                         oneconfig->pos -= 1 ;//updates pos of one position to the left
                     break;
                 case 'R':
-                    if(oneconfig->pos >= strlen(oneconfig->String)){
+                    //printf("strlen(oneconfig->String): %lu \n",strlen(oneconfig->String));
+                    if(oneconfig->pos >= strlen(oneconfig->String)-1){
                         string[strlen(string)] = '_';
-                        strcat(string, "\0");
+                        string[strlen(string)+1] = '\0';
                     }
                     oneconfig->pos += 1;
                     break;
@@ -341,7 +350,7 @@ config *ND_Step(config **c, TM tm, int *accept){
                     printf("Default case: not readable head movement");
                     break;
             }
-            printf("AFTER: %s\tS:%d\tH:%d\n", oneconfig->String, oneconfig->state, oneconfig->pos);
+            //printf("AFTER:\t%s\tS:%d\tH:%d\n", oneconfig->String, oneconfig->state, oneconfig->pos);
             //linking different paths as list;
             if(flag == 0) {
                 root = oneconfig;
@@ -350,23 +359,26 @@ config *ND_Step(config **c, TM tm, int *accept){
             else
                 ADDconfig(root, oneconfig);
             choice = choice->next;
-            printf("\n");
+            //printf("\n");
         }
         free((*c)->String);
-        
         return root;
     }
     else {
+        //printf("STRING:\t%s\tS:%d\tH:%d\tDELETED\n\n",(*c)->String,(*c)->state,(*c)->pos);
         free((*c)->String);
         if((*c)->prev != NULL) {
             (*c)->prev =  (*c)->next;
-            (*c)->next->prev = (*c)->prev;
+            if((*c)->next != NULL)
+                (*c)->next->prev = (*c)->prev;
         }
         else {
             root = (*c)->next;
-            (*c)->next->prev = NULL;
+            if((*c)->next != NULL)
+                (*c)->next->prev = NULL;
         }
         free(*c);
+        *c = root;
         return NULL;
     }
 }
@@ -374,33 +386,26 @@ config *ND_Step(config **c, TM tm, int *accept){
 /* ---------------------------------------------------------------------- */
 int main(int argc, char *argv[])
 {
+    clock_t tStart = clock();
     char c;
     TM machine;
-    freopen(argv[1], "r", stdin); //check from lldb
+    freopen(argv[4], "r", stdin); //check from lldb
     init_TM(&machine);
     LoadTM(&machine);
     c = fgetc(stdin);   //read character to see if it is EOF
     ungetc(c, stdin);   //puts back the read character
     
-    machine.snapshot->String = LOAD_STRING();         //load one string
-    
-    machine.snapshot->pos = 1;  //strings have this format _"string"_, pos=1;
-    
-    ND_Transition(&machine);
-    
-    /*
     while(c != EOF) {
-         machine.snapshot->String = LOAD_STRING();         //load one string
+        machine.snapshot->String = LOAD_STRING();         //load one string
+        machine.snapshot->pos = 1;  //strings have this format _"string"_, pos=1;
+        ND_Transition(&machine);
      
-         machine.snapshot->pos = 1;  //strings have this format _"string"_, pos=1;
-     
-         ND_Transition(&machine);
-     
-        //VisualizeString(machine.snapshot.String);
-        c = fgetc(stdin);                      //needed to check the first character of the string
+        c = fgetc(stdin);           //needed to check the first character of the string
+        if(c != EOF)
+            printf("\n");
         ungetc(c, stdin);
     }
-     */
-    if(c == EOF)
-        printf("EOF reached: %d\n", c);
+    printf("\nTime taken: %2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    
+    return 0;
 }
